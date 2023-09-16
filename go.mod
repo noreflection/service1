a3e4645dae710b5ca98173cfd7441a615d76650e
Nikita Shapovalov <a191a191a@gmail.com>
2023-09-16 21:42:40 +0300

    cnages

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module service1
 
 go 1.21
+
+require github.com/streadway/amqp v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,43 +1,96 @@
-// microservice1.go
+// Microservice 1: main.go
 
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
+	"encoding/json"
+	"github.com/streadway/amqp"
 	"log"
 	"net/http"
 )
 
-// Define a global variable to store the message temporarily
-var message string
-
-// Define an endpoint to receive messages via POST requests
-func receiveMessageHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusInternalServerError)
-		return
-	}
-	message = string(body)
-	fmt.Fprintf(w, "Message received: %s", message)
-
-	// Pass the message to Microservice 2
-	passMessageToMicroservice2(message)
+// Message Define a struct to represent the message
+type Message struct {
+	Text string `json:"text"`
 }
 
-// Function to pass the message to Microservice 2
-func passMessageToMicroservice2(message string) {
-	// Send an HTTP POST request to Microservice 2 with the message
-	_, err := http.Post("http://microservice2:8080/receive", "application/json", bytes.NewBuffer([]byte(message)))
-	if err != nil {
-		log.Printf("Failed to pass message to Microservice 2: %v", err)
-	}
-}
+// RabbitMQ connection parameters
+const (
+	rabbitMQURL = "amqp://guest:guest@localhost:5672/"
+	queueName   = "messages"
+)
 
 func main() {
 	http.HandleFunc("/receive", receiveMessageHandler)
-	//fmt.Print("hey")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func receiveMessageHandler(w http.ResponseWriter, r *http.Request) {
+	var message Message
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&message); err != nil {
+		http.Error(w, "Failed to decode message", http.StatusBadRequest)
+		return
+	}
+
+	// Publish the message to RabbitMQ
+	err := publishToRabbitMQ(message)
+	if err != nil {
+		http.Error(w, "Failed to send message to RabbitMQ", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Message received and sent to RabbitMQ"))
+}
+
+func publishToRabbitMQ(message Message) error {
+	conn, err := amqp.Dial(rabbitMQURL)
+	if err != nil {
+		log.Printf("cannot connect")
+		return err
+	}
+	defer func(conn *amqp.Connection) {
+		_ = conn.Close()
+	}(conn)
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer func(ch *amqp.Channel) {
+		_ = ch.Close()
+	}(ch)
+
+	q, err := ch.QueueDeclare(
+		queueName, // Queue name
+		false,     // Durable
+		false,     // Delete when unused
+		false,     // Exclusive
+		false,     // No-wait
+		nil,       // Arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	body, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	err = ch.Publish(
+		"",     // Exchange
+		q.Name, // Routing key
+		false,  // Mandatory
+		false,  // Immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        body,
+		})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
